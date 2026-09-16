2b99a48234a930fe09b8ffe4e9e459c1c3ee94b4
shinichikudo18 <59715569+shinichikudo18@users.noreply.github.com>
2026-09-16 03:29:59 -0300

    chore: bump Netheris GPS to v6.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cl.netheris.gps"
         minSdk = 26
         targetSdk = 35
-        versionCode = 60
-        versionName = "6.0.0"
+        versionCode = 61
+        versionName = "6.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
