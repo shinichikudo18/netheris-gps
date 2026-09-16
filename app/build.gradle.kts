20783c1dfa6d57f69c6c5570c2eba27a1587ad67
shinichikudo18 <59715569+shinichikudo18@users.noreply.github.com>
2026-09-16 00:31:28 -0300

    feat: add MapLibre SDK for APK v1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,9 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui-viewbinding")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("org.maplibre.gl:android-sdk:13.6.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
